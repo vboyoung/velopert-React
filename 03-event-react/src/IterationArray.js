0639ccccecc2a75dee1d7485fbcabdc60fc1a8e2
vboyoung <vboyoungh@gamil.com>
2021-07-02 17:20:29 +0900

    map

---
+++ 03-event-react/src/IterationArray.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const IterationArray = () => {
+
+    const names = ['눈사람', '얼음', '눈', '바람']
+    const nameList  = names.map((name, index) => <li key={index}>{name}</li>)
+
+    return(
+        <>
+          <ul>{nameList}</ul>
+        </>
+    )
+}
+
+
+
+export default IterationArray
