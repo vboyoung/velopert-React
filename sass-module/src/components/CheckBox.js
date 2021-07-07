b9572c9fe6e225f700b08e659130e0487b6b81d4
vboyoung <vboyoungh@gamil.com>
2021-07-07 15:12:51 +0900

    styled-componets

---
+++ sass-module/src/components/CheckBox.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md'
+import styles from './CheckBox.module.scss'
+
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+const CheckBox = ({children, checked, ...rest}) => {
+
+    return (
+        <div className={cx('checkbox')}>
+        <label>
+          <input type="checkbox" checked={checked} {...rest} />
+          <div className={cx('icon')}>
+            {checked ? (
+              <MdCheckBox className={cx('checked')} />
+            ) : (
+              <MdCheckBoxOutlineBlank />
+            )}
+          </div>
+        </label>
+        <span>{children}</span>
+      </div>
+    )
+}
+
+export default CheckBox
